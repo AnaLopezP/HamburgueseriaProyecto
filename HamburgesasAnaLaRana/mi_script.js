cd2126146e76a33735feee2cd318c01e072468f8
Ana López <anamuyamiga@gmail.com>
2023-11-22 13:35:17 +0100

    lsdjlsd

--- HamburgesasAnaLaRana/mi_script.js
+++ HamburgesasAnaLaRana/mi_script.js
@@ -15,3 +15,21 @@ function verificarInicioSesion() {
         alert('Error de inicio de sesión. Verifica tus credenciales.');
     }
 }
+
+function registrarUsuario() {
+    var newUsername = document.getElementById('newUsername').value;
+    var newPassword = document.getElementById('newPassword').value;
+
+    // Verificar si el usuario ya existe
+    var usuarioExistente = usuarios.find(function (usuario) {
+        return usuario.username === newUsername;
+    });
+
+    if (usuarioExistente) {
+        alert('El nombre de usuario ya está en uso. Por favor, elige otro.');
+    } else {
+        // Agregar nuevo usuario al array
+        usuarios.push({ username: newUsername, password: newPassword });
+        alert('¡Registro exitoso!');
+    }
+}
