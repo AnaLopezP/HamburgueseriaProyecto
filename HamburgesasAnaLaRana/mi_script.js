00c368237ed9388d20b840137ba21c569b7cf58c
Ana López <anamuyamiga@gmail.com>
2023-11-22 13:33:52 +0100

    funcion inicio sesion

--- HamburgesasAnaLaRana/mi_script.js
+++ HamburgesasAnaLaRana/mi_script.js
@@ -1,6 +1,17 @@
-<script>
-function iniciarSesion() {
-    //para enviar los datos del formulario mediante JavaScript
-    alert('Iniciar Sesión: Implementa la lógica aquí');
+let usuarios_registrados = []
+
+function verificarInicioSesion() {
+    let username = document.getElementById('username').value;
+    let password = document.getElementById('password').value;
+
+    // Verificar las credenciales
+    let usuarioEncontrado = usuarios.find(function (usuario) {
+        return usuario.username === username && usuario.password === password;
+    });
+
+    if (usuarioEncontrado) {
+        alert('¡Inicio de sesión exitoso!');
+    } else {
+        alert('Error de inicio de sesión. Verifica tus credenciales.');
+    }
 }
-</script>
